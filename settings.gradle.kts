b279a6f74a64c6526186d9db8f1259874b4cb9bd
namasudraabhijit978-web <namasudraabhijit978@gmail.com>
2026-06-24 17:48:53 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,4 @@
 rootProject.name = "Chronos"
 
 include(":app")
-include(":core:security")
-include(":core:database")
-include(":core:totp")
-include(":feature:dashboard")
-include(":feature:scanner")
-include(":feature:settings")
-include(":feature:auth")
+include(":core")
