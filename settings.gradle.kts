464563dc6844201d8a380b696905c022081b8001
namasudraabhijit978-web <namasudraabhijit978@gmail.com>
2026-06-24 17:16:37 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "Chronos"
+
+include(":app")
+include(":core:security")
+include(":core:database")
+include(":core:totp")
+include(":feature:dashboard")
+include(":feature:scanner")
+include(":feature:settings")
+include(":feature:auth")
